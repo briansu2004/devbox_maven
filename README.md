a2f5df1e47a1266ec50e927d8a52d7efaa6ac17e
Brian Su <briansu2004@hotmail.com>
2024-04-01 06:56:18 -0400

    $

--- README.md
+++ README.md
@@ -2,4 +2,8 @@
 
 My Udemy Course Maven Workspace
 
+## Java Web App 001: commons-uploads
+
 ## Maven Fix Vulnerability 001: commons-collections
+
+## Maven Fix Vulnerability 002: commons-fileupload
---
+++ init_j2ee_app_event.bat
@@ -0,0 +1,5 @@
+@cls
+
+cd c:\devbox_maven
+
+mvn org.apache.maven.plugins:maven-archetype-plugin:3.1.2:generate -DarchetypeArtifactId="maven-archetype-j2ee-simple" -DarchetypeGroupId="org.apache.maven.archetypes" -DarchetypeVersion="1.4" -DgroupId="com.sutek" -DartifactId="java-j2ee-app-event" -DinteractiveMode=false
